fd22053973c66fdea7ac7008e49545b994bbdfea
lfav07 <lfavieira07@gmail.com>
2026-09-02 16:43:06 -0300

    refactor: introduce new gradient over white plain color

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -18,9 +18,9 @@ const links = [
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 border-b bg-background/80 backdrop-blur-sm">
-      <div className="mx-auto flex h-14 max-w-7xl items-center px-4">
-        <NavLink to="/" className="mr-6 text-lg font-bold">
+    <header className="sticky top-0 z-50 border-b bg-linear-to-r from-[#f79d00] to-[#64f38c] backdrop-blur-sm">
+      <div className="mx-auto flex justify-between gap-12 h-14 max-w-7xl items-center px-4">
+        <NavLink to="/" className="mr-6  text-lg font-bold">
           Carnode
         </NavLink>
         <NavigationMenu>
